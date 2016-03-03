dc18841675cdff9d7dba01d39cf636c98a11a05f
Leveng93 <Ogorelkov_Sergey@outlook.com>
2016-03-03 22:03:30 +0600

    ViewModel + DataMonitor

--- NetworkMonitor/MainViewModel.cs
+++ NetworkMonitor/MainViewModel.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
 
 namespace NetworkMonitor
 {
     class MainViewModel
     {
-        
+        NetworkDataMonitor monitor = NetworkDataMonitor.Instance;
+
+        public async void StartNetworkMonitor ()
+        {
+            IPHostEntry ipHost = await Dns.GetHostEntryAsync(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[4];   // Дать пользователю выбрать IP в GUI;
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+            await monitor.StartAsync(ipAddr, ipEndPoint);
+        }
     }
 }
--- NetworkMonitor/NetworkDataMonitor.cs
+++ NetworkMonitor/NetworkDataMonitor.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using NetworkMonitor.Packets;
+using System;
 using System.Net;
 using System.Net.Sockets;
-using NetworkMonitor.Packets;
+using System.Threading.Tasks;
 
 namespace NetworkMonitor
 {
@@ -15,9 +14,12 @@ namespace NetworkMonitor
         public delegate void PackedRecived(PacketIP packet);
         public event PackedRecived PacketReceivedEvent;
 
-        Socket mainSocket;
-        byte[] buffer;
+        Socket mainSocket;  // Основной сокет.
+        byte[] buffer;      // Буффер, в который считывается пакет.
 
+        /// <summary>
+        /// Отображение состояния сетевого монитора.
+        /// </summary>
         public bool Started { get; private set; }
 
         private static readonly Lazy<NetworkDataMonitor> _instance = new Lazy<NetworkDataMonitor>(() => new NetworkDataMonitor());
@@ -28,7 +30,12 @@ namespace NetworkMonitor
         /// </summary>
         public static NetworkDataMonitor Instance { get { return _instance.Value; } }
 
-        public void Start(IPAddress ipAddr, IPEndPoint ipEndPoint)   // Запускает мониторинг в синхронном режиме. 
+        /// <summary>
+        /// Запуск мониторинга в синхронном режиме.
+        /// </summary>
+        /// <param name="ipAddr"></param>
+        /// <param name="ipEndPoint"></param>
+        public void Start(IPAddress ipAddr, IPEndPoint ipEndPoint) 
         {
             if (Started) return;
             try
@@ -47,7 +54,7 @@ namespace NetworkMonitor
                     {
                         int received = mainSocket.Receive(buffer, 0, buffer.Length, SocketFlags.None);  // Считываем пакет в буффер.
                         OnPacketReceivedEvent(new PacketIP(buffer, received));  // Создаем новый IP пакет, запускаем событие (рассылаем пакет подписчикам).
-                        Array.Clear(buffer, 0, received); // Очищаем пакет.
+                        Array.Clear(buffer, 0, received); // Очищаем буффер.
                     }
                 }
             }
@@ -58,17 +65,21 @@ namespace NetworkMonitor
             }
         }
 
+        // Запуск мониторинга в асинхронном режиме.
         public async Task StartAsync(IPAddress ipAddr, IPEndPoint ipEndPoint)
         {
             await Task.Run(() => Start(ipAddr, ipEndPoint));
         }
-
 
         public void Stop()
         {
             Started = false;  
         }
 
+        /// <summary>
+        /// Рассылка полученного пакета. 
+        /// </summary>
+        /// <param name="packet"></param>
         void OnPacketReceivedEvent (PacketIP packet)
         {
             PackedRecived pr;
